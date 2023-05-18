6d8467b48fcf1244c81b46b766a337fddb6385b4
florybal <soonluck8@gmail.com>
2023-05-17 23:33:04 -0300

    adicionando a equação da reta

--- src/triangulo.cpp
+++ src/triangulo.cpp
@@ -1,12 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <cmath>
+#include <string>
 
 int main()
 {
+   // y -y1/ yb - ya = x - x1/ x2 - x1 
+
     bool triangulo;
     double DAC, DBC, DAB;
     float Ax, Ay, Bx,  By, Cx, Cy;
+    //std::string x, y;
     
     //coordenadas em pixels na tela
     std::cout << "De as coordenadas do triangulo \n A:" << std::endl;
@@ -20,6 +24,26 @@ int main()
     DAC = sqrt(pow(Ax-Cx,2) +  pow(Ay-Cy,2) ); //a
     DBC = sqrt(pow(Bx-Cx,2) +  pow(By-Cy,2) ); //b
     DAB = sqrt(pow(Ax-Bx,2) +  pow(Ay-By,2) ); //c
+
+    std::cout<<"Equações da reta: "<<std::endl;
+
+    // equação da reta A pelo seguemento AB = C
+    std::cout << (Bx - Ax)*(Ay)<<"y"
+    << - (By - Ay)*(Ax)<<"x" <<std::endl; 
+    
+    //equação da reta BC = B 
+    std::cout<< (Cx - Bx)*(- By)<<"y" 
+    << - (Cy - By)*(Cx) << "x" <<std::endl;
+
+    // equação da reta A pelo seguemento AC = A
+    std::cout << (Cx - Ax)*(Ay) <<  "y"
+    << - (Cy - Ay)*(Cx) << "x" <<std::endl;
+
+    //auto Rbc = (Cx - Bx)*(y - By) - (Cy - By)*(x - Cx); // equação da reta A pelo seguemento BC = B
+
+    //std::cout << Rac <<std::endl << Rbc <<std::endl <<Rab <<std::endl; 
+    
+    //std::cout << Rac + Rac1 <<std::endl;
 
     if((DAB + DBC > DAC) && (DAC + DAB > DBC) && (DBC + DAB > DAC))
     {
