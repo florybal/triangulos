694eb87d3f97a886393d378cc524202675310a79
florybal <soonluck8@gmail.com>
2023-09-05 00:40:28 -0300

    organizando em classes o codigo

---
+++ src/triangulo.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class triangle
+{
+    public:
+
+
+    private:
+        sf::RenderWindow* _window;
+    private:
+        void desenha();
+
+
+        Mediatriz();
+        ponto_med();
+        bool triangulo();
+        Mediana();
+        baricentro();
+
+};
